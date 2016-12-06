bbd73aa752259f7bda1832da2f936743599a04bf
LeslieRuckman <leslieruckman@gmail.com>
2016-12-06 00:08:48 -0500

    updating with working twitter code

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+  facebookAuth: {
+    clientID: '1363062387040174',
+    clientSecret: 'f05c2941dc30b4309d988c8d68f3dfb5',
+    callbackURL: 'http://localhost:4000/auth/facebook/callback',
+  },
+  twitterAuth: {
+    consumerKey: 'wDiwkdYWVmjXuTqjLQ9DyYF2q',
+    consumerSecret: '4d1lZoTBbOnEXxtssLd9RLSEl4XVBGvFOQlLveUgUxIwvo2mHf',
+    callbackURL: 'http://localhost:4000/auth/twitter/callback',
+  },
+  MONGODB_URI: 'mongodb://heroku_7zr2f5k5:1m4492pn3uptpf4oj6cn4smfqg@ds059516.mlab.com:59516/heroku_7zr2f5k5'
+}
